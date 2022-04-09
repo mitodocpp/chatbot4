39ccd005662543dca171ee202a914517f3ae6f64
the programmer <88006978+mitodocpp@users.noreply.github.com>
2022-04-09 15:23:17 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+import random
+   
+print("hi, what`s your name?")
+name = input()
+time.sleep(random.randint(3, 5))
+if name == "Villinger":
+     print("oi priminho, espero que goste")
+else:
+    print("hi, " + name)
+ print("my name is ???")
+ time.sleep(1)
+ print("my name is ??")
+ time.sleep(1)
+ print("wait, what my name")
+ ia_name = input("insert my name:")
+ time.sleep(5)
+ print("more soon...")"hi, " + name)
